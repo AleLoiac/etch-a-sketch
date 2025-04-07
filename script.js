1589fc1561e03c53be69b4d38e0deec51640ee61
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-07 15:43:24 +0200

    Randomize colors

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ for (let i = 0; i < 256; i++) {
 }
 const squareNodes = document.querySelectorAll(".hover");
 
+const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 container.addEventListener("mouseover", (e) => {
     const target = e.target;
 
     if (target.classList.value === "hover") {
-        target.style.background = "black";
+        target.style.background = getRandomColor();
     }
 })
