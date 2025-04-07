b7d0a13e9dbc882a3341ab8746aa8207184a2e2a
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-07 15:34:03 +0200

    Add hovering effect

--- script.js
+++ script.js
@@ -3,4 +3,14 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement("div");
     container.appendChild(squareDiv);
+    squareDiv.classList.add("hover");
 }
+const squareNodes = document.querySelectorAll(".hover");
+
+container.addEventListener("mouseover", (e) => {
+    const target = e.target;
+
+    if (target.classList.value === "hover") {
+        target.style.background = "black";
+    }
+})
