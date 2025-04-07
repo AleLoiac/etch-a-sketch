7fd82a26707a26bffe4af46ecf9bcf2899ecb9c7
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-04-07 12:46:18 +0200

    Add grid generation

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 256; i++) {
+    const squareDiv = document.createElement("div");
+    container.appendChild(squareDiv);
+}
